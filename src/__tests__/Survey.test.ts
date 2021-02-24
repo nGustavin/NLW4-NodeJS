dcb1f6670f152a5edac53779289bb2b6593dad21
Not Your Bussiness Bitch <gust4f3rn@gmail.com>
2021-02-24 11:27:49 -0300

    surveys passed to test of index

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -16,6 +16,18 @@ describe("Surveys", () => {
     })
 
     expect(response.status).toBe(201)
+    expect(response.body).toHaveProperty("id")
+  })
+
+  it("Should be able to get all surveys", async () => {
+    await request(app).post('/surveys').send({
+      title: 'survey title test2',
+      description: 'survey description test with a lot of caracters2'
+    })
+
+    const response = await request(app).get('/surveys')
+
+    expect(response.body.length).toBe(2)
   })
 })
 

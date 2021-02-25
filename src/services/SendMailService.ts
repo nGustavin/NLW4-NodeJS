26d11346d67ccd2221e0c25286bd06f583da0288
Not Your Bussiness Bitch <gust4f3rn@gmail.com>
2021-02-25 10:13:27 -0300

    handlebar template variables parsed to mail style

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,7 +1,7 @@
 import nodemailer, { Transporter } from "nodemailer"
-
-
-
+import {resolve} from 'path'
+import handlebars from 'handlebars'
+import fs from 'fs'
 class SendMailService {
   private client: Transporter
   constructor() {
@@ -21,10 +21,21 @@ class SendMailService {
   }
 
   async execute(to: string, subject: string, body: string) {
+
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs")
+    const templateFileContent = fs.readFileSync(npsPath).toString("utf-8")
+    const mailTemplateParse = handlebars.compile(templateFileContent)
+
+    const html = mailTemplateParse({
+      name: to,
+      title: subject,
+      description: body
+    })
+
     const message = await this.client.sendMail({
       to,
       subject,
-      html: body,
+      html, 
       from: "NPS <noreply@nps.com.br>"
     })
 

870d05617fced63774edc26a0c6e0e939d13089a
Not Your Bussiness Bitch <gust4f3rn@gmail.com>
2021-02-25 09:27:27 -0300

    sendMail controller and route created

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,3 +1,44 @@
+import { Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
+import { SurveysRepository } from '../repositories/SurveysRepository'
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository'
+import { UsersRepository } from '../repositories/UsersRepository'
+
 export default {
-  
+  async execute(req: Request, res: Response){
+    const { email, survey_id } = req.body
+
+    const usersRepository = getCustomRepository(UsersRepository)
+    const surveysRepository = getCustomRepository(SurveysRepository)
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
+
+    const userAlreadyExists = await usersRepository.findOneOrFail({ email })
+
+    if(!userAlreadyExists){
+      return res.status(400).json({error: "User does not exist"})
+    }
+
+    const surveyAlreadyExists = await surveysRepository.findOneOrFail({ id: survey_id })
+
+    if(!surveyAlreadyExists){
+      return res.status(400).json({error: "Survey does not exist"})
+    }
+
+    const surveyUser = await surveysUsersRepository.create({
+      user_id: userAlreadyExists.id,
+      survey_id
+    })
+
+    await surveysUsersRepository.save(surveyUser)
+
+    return res.json(surveyUser)
+  }, 
+
+  async listMail(req: Request, res: Response){
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
+
+    const surveys = await surveysUsersRepository.find();
+
+    return res.status(200).json(surveys)
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import {Router } from 'express'
 import surveysController from './controllers/SurveysController'
 import usersController from './controllers/UsersController'
+import mailController from './controllers/SendMailController'
 
 const routes = Router()
 
@@ -10,5 +11,8 @@ routes.get('/users/:id', usersController.show)
 
 routes.post('/surveys', surveysController.create)
 routes.get('/surveys', surveysController.index)
+
+routes.post('/sendMail' , mailController.execute)
+routes.get('/sendMail' , mailController.listMail)
 
 export default routes
